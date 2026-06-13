327e78bd500d1a17bbd27d277e2b46a1fa37a3a2
hoanglam889 <lamphan3107@gmail.com>
2026-06-13 21:33:11 +0700

    db

--- clinic_flow_erp.sql
+++ clinic_flow_erp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th6 05, 2026 lúc 06:45 PM
+-- Thời gian đã tạo: Th6 13, 2026 lúc 04:32 PM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
@@ -38,6 +38,15 @@ CREATE TABLE `appointments` (
   `priority_score` int(11) DEFAULT 1,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `appointments`
+--
+
+INSERT INTO `appointments` (`id`, `qr_code`, `patient_id`, `doctor_profile_id`, `appointment_date`, `appointment_time`, `status`, `priority_score`, `created_at`) VALUES
+(13, 'HT-APPT-20260613-0B5397A6', 2, 1, '2026-06-13', '17:00:00', 'DONE', 4, '2026-06-13 13:54:46'),
+(14, 'HT-APPT-20260613-ED7E14EB', 2, 1, '2026-06-14', '14:00:00', 'DONE', 6, '2026-06-13 14:05:06'),
+(15, 'HT-APPT-20260613-43DC689E', 4, 1, '2026-06-14', '14:00:00', 'DONE', 6, '2026-06-13 14:29:43');
 
 -- --------------------------------------------------------
 
@@ -83,6 +92,46 @@ INSERT INTO `doctor_profiles` (`id`, `user_id`, `specialty_id`, `full_name`, `av
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `doctor_schedules`
+--
+
+CREATE TABLE `doctor_schedules` (
+  `id` int(11) NOT NULL,
+  `doctor_profile_id` int(11) NOT NULL,
+  `shift_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `max_patients` int(11) DEFAULT 5,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `doctor_schedules`
+--
+
+INSERT INTO `doctor_schedules` (`id`, `doctor_profile_id`, `shift_id`, `date`, `max_patients`, `created_at`) VALUES
+(1, 1, 1, '2026-06-08', 3, '2026-06-05 17:35:53'),
+(2, 1, 2, '2026-06-08', 3, '2026-06-05 17:35:53'),
+(3, 1, 3, '2026-06-08', 3, '2026-06-05 17:35:53'),
+(4, 1, 4, '2026-06-08', 3, '2026-06-05 17:35:53'),
+(5, 2, 5, '2026-06-07', 4, '2026-06-05 17:35:53'),
+(6, 2, 6, '2026-06-07', 4, '2026-06-05 17:35:53'),
+(7, 2, 7, '2026-06-07', 4, '2026-06-05 17:35:53'),
+(8, 2, 8, '2026-06-07', 4, '2026-06-05 17:35:53'),
+(9, 3, 9, '2026-06-08', 3, '2026-06-05 17:35:53'),
+(10, 3, 10, '2026-06-08', 3, '2026-06-05 17:35:53'),
+(11, 3, 11, '2026-06-08', 3, '2026-06-05 17:35:53'),
+(12, 3, 12, '2026-06-08', 3, '2026-06-05 17:35:53'),
+(13, 4, 1, '2026-06-09', 4, '2026-06-05 17:35:53'),
+(14, 4, 2, '2026-06-09', 4, '2026-06-05 17:35:53'),
+(15, 4, 5, '2026-06-09', 4, '2026-06-05 17:35:53'),
+(16, 4, 6, '2026-06-13', 4, '2026-06-05 17:35:53'),
+(17, 1, 9, '2026-06-13', 3, '0000-00-00 00:00:00'),
+(19, 2, 10, '2026-06-13', 3, '0000-00-00 00:00:00'),
+(20, 1, 6, '2026-06-14', 3, '2026-06-13 14:04:16');
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `invoices`
 --
 
@@ -95,6 +144,15 @@ CREATE TABLE `invoices` (
   `paid_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `invoices`
+--
+
+INSERT INTO `invoices` (`id`, `appointment_id`, `total_amount`, `status`, `payment_method`, `paid_at`, `created_at`) VALUES
+(13, 13, 150000.00, 'PAID', 'CASH', '2026-06-13 13:55:56', '2026-06-13 13:54:46'),
+(14, 14, 150000.00, 'PAID', 'CASH', '2026-06-13 14:30:54', '2026-06-13 14:05:06'),
+(15, 15, 150000.00, 'UNPAID', 'CASH', NULL, '2026-06-13 14:29:43');
 
 -- --------------------------------------------------------
 
@@ -110,6 +168,15 @@ CREATE TABLE `medical_records` (
   `notes` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `medical_records`
+--
+
+INSERT INTO `medical_records` (`id`, `appointment_id`, `symptoms`, `diagnosis`, `notes`, `created_at`) VALUES
+(2, 13, 'abc', 'abc', 'abc', '2026-06-13 13:55:35'),
+(3, 14, 'abcd', 'abcd', 'abcd', '2026-06-13 14:30:26'),
+(4, 15, 'HT-APPT-20260613-43DC689E', 'HT-APPT-20260613-43DC689E', 'HT-APPT-20260613-43DC689E', '2026-06-13 14:31:08');
 
 -- --------------------------------------------------------
 
@@ -150,7 +217,9 @@ CREATE TABLE `patients` (
 --
 
 INSERT INTO `patients` (`id`, `patient_account_id`, `relationship`, `cccd`, `full_name`, `dob`, `gender`, `phone`, `address`, `created_at`) VALUES
-(2, 1, 'Bản thân', '012345678901', 'Hoàng Lâm', '1995-10-20', 'MALE', '0987654321', '123 Đường Ba Đình, Hà Nội', '2026-06-05 09:15:10');
+(2, 1, 'Bản thân', '012345678901', 'Hoàng Lâm', '1995-10-20', 'MALE', '0987654321', '333 ấp 10, xã Rạch Kiến, Tây Ninh', '2026-06-05 09:15:10'),
+(3, NULL, 'Vợ/Chồng', NULL, 'Phạm Thu Hà', '2003-12-21', 'FEMALE', '0797551612', NULL, '2026-06-12 17:08:21'),
+(4, 4, 'Bản thân', NULL, 'Nguyễn Văn Lâm', '2007-11-13', 'MALE', '0797551612', '333 ấp 10, Rạch Kiến, Tây Ninh', '2026-06-13 14:29:26');
 
 -- --------------------------------------------------------
 
@@ -173,7 +242,8 @@ CREATE TABLE `patient_accounts` (
 INSERT INTO `patient_accounts` (`id`, `phone`, `password_hash`, `is_active`, `created_at`) VALUES
 (1, '0901234567', '$2b$10$wO3.Vj.8x1...dummyhash123456789', 1, '2026-06-05 09:40:27'),
 (2, '0987654321', '$2b$10$xP4.Wk.9y2...dummyhash987654321', 1, '2026-06-05 09:40:27'),
-(3, '0911222333', '$2b$10$yQ5.Zl.0z3...dummyhash112233445', 0, '2026-06-05 09:40:27');
+(3, '0911222333', '$2b$10$yQ5.Zl.0z3...dummyhash112233445', 0, '2026-06-05 09:40:27'),
+(4, '0797551612', '1', 0, '2026-06-05 09:40:27');
 
 -- --------------------------------------------------------
 
@@ -183,13 +253,28 @@ INSERT INTO `patient_accounts` (`id`, `phone`, `password_hash`, `is_active`, `cr
 
 CREATE TABLE `shifts` (
   `id` int(11) NOT NULL,
-  `doctor_profile_id` int(11) NOT NULL,
-  `shift_date` date NOT NULL,
+  `name` varchar(50) NOT NULL,
   `start_time` time NOT NULL,
-  `end_time` time NOT NULL,
-  `max_patients` int(11) DEFAULT 5,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `end_time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `shifts`
+--
+
+INSERT INTO `shifts` (`id`, `name`, `start_time`, `end_time`) VALUES
+(1, 'Ca Sáng 1', '08:00:00', '09:00:00'),
+(2, 'Ca Sáng 2', '09:00:00', '10:00:00'),
+(3, 'Ca Sáng 3', '10:00:00', '11:00:00'),
+(4, 'Ca Sáng 4', '11:00:00', '12:00:00'),
+(5, 'Ca Chiều 1', '13:00:00', '14:00:00'),
+(6, 'Ca Chiều 2', '14:00:00', '15:00:00'),
+(7, 'Ca Chiều 3', '15:00:00', '16:00:00'),
+(8, 'Ca Chiều 4', '16:00:00', '17:00:00'),
+(9, 'Ca Tối 1', '17:00:00', '18:00:00'),
+(10, 'Ca Tối 2', '18:00:00', '19:00:00'),
+(11, 'Ca Tối 3', '19:00:00', '20:00:00'),
+(12, 'Ca Tối 4', '20:00:00', '21:00:00');
 
 -- --------------------------------------------------------
 
@@ -237,10 +322,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `phone`, `password_hash`, `role`, `is_active`, `created_at`) VALUES
-(4, '0000004', 'dummy_password', 'DOCTOR', 1, '2026-06-05 16:40:33'),
-(5, '0000005', 'dummy_password', 'DOCTOR', 1, '2026-06-05 16:40:33'),
-(6, '0000006', 'dummy_password', 'DOCTOR', 1, '2026-06-05 16:40:33'),
-(7, '0000007', 'dummy_password', 'DOCTOR', 1, '2026-06-05 16:40:33');
+(4, '004', '1', 'DOCTOR', 1, '2026-06-05 16:40:33'),
+(5, '005', '1', 'DOCTOR', 1, '2026-06-05 16:40:33'),
+(6, '006', '1', 'DOCTOR', 1, '2026-06-05 16:40:33'),
+(7, '007', '1', 'DOCTOR', 1, '2026-06-05 16:40:33'),
+(8, '008', '1', 'STAFF', 1, '2026-06-05 16:40:33');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -270,6 +356,14 @@ ALTER TABLE `doctor_profiles`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `user_id` (`user_id`),
   ADD KEY `fk_doctor_profiles_specialty` (`specialty_id`);
+
+--
+-- Chỉ mục cho bảng `doctor_schedules`
+--
+ALTER TABLE `doctor_schedules`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_doctor_schedules_doctor` (`doctor_profile_id`),
+  ADD KEY `fk_doctor_schedules_shift` (`shift_id`);
 
 --
 -- Chỉ mục cho bảng `invoices`
@@ -311,8 +405,7 @@ ALTER TABLE `patient_accounts`
 -- Chỉ mục cho bảng `shifts`
 --
 ALTER TABLE `shifts`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_shifts_doctor_profile` (`doctor_profile_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Chỉ mục cho bảng `specialties`
@@ -335,7 +428,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT cho bảng `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT cho bảng `appointment_status_logs`
@@ -350,16 +443,22 @@ ALTER TABLE `doctor_profiles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT cho bảng `doctor_schedules`
+--
+ALTER TABLE `doctor_schedules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
 -- AUTO_INCREMENT cho bảng `invoices`
 --
 ALTER TABLE `invoices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT cho bảng `medical_records`
 --
 ALTER TABLE `medical_records`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `notifications`
@@ -371,19 +470,19 @@ ALTER TABLE `notifications`
 -- AUTO_INCREMENT cho bảng `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `patient_accounts`
 --
 ALTER TABLE `patient_accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `shifts`
 --
 ALTER TABLE `shifts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT cho bảng `specialties`
@@ -395,7 +494,7 @@ ALTER TABLE `specialties`
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
@@ -423,6 +522,13 @@ ALTER TABLE `doctor_profiles`
   ADD CONSTRAINT `fk_doctor_profiles_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
+-- Các ràng buộc cho bảng `doctor_schedules`
+--
+ALTER TABLE `doctor_schedules`
+  ADD CONSTRAINT `fk_doctor_schedules_doctor` FOREIGN KEY (`doctor_profile_id`) REFERENCES `doctor_profiles` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_doctor_schedules_shift` FOREIGN KEY (`shift_id`) REFERENCES `shifts` (`id`);
+
+--
 -- Các ràng buộc cho bảng `invoices`
 --
 ALTER TABLE `invoices`
@@ -445,12 +551,6 @@ ALTER TABLE `notifications`
 --
 ALTER TABLE `patients`
   ADD CONSTRAINT `fk_patients_patient_account` FOREIGN KEY (`patient_account_id`) REFERENCES `patient_accounts` (`id`) ON DELETE SET NULL;
-
---
--- Các ràng buộc cho bảng `shifts`
---
-ALTER TABLE `shifts`
-  ADD CONSTRAINT `fk_shifts_doctor_profile` FOREIGN KEY (`doctor_profile_id`) REFERENCES `doctor_profiles` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
